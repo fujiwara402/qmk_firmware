8609d38011efd42c80ab843cce92148b6a241493
fujiwara402 <fuj.t601216@gmail.com>
2017-07-07 22:09:09 +0900

    Add so many ESC

--- keyboards/ergodox/keymaps/chrml/keymap.c
+++ keyboards/ergodox/keymaps/chrml/keymap.c
@@ -24,39 +24,39 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
  * | Ctrl   |   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |   ;  |   '"   |
  * |--------+------+------+------+------+------| Kana |           | Eisu |------+------+------+------+------+--------|
- * | LShift |Z/Ctrl|   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |   /  | RShift |
+ * | LShift |Z/Ctrl|   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |   /  |   Alt  |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
  *   |Grv/L1| LGui |AltShf| LEFT | RIGHT|                                       |  Up  | Down |   [  |   ]  | L2   |
  *   `----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,-------------.  
- *                                        | App  | LGui |       | Alt  |Ctrl/Esc|
- *                                 ,------|------|------|       |------+--------+------.
- *                                 |      |      | Home |       | PgUp |        |      |
- *                                 | Space|Backsp|------|       |------|  Tab   |Enter |
- *                                 |      |ace   | End  |       | PgDn |        |      |
- *                                 `--------------------'       `----------------------'
+ *                                        | Esc  | Esc  |       | Esc  | Esc  |
+ *                                 ,------|------|------|       |------+------+------.
+ *                                 |      |      | Esc  |       | Esc  |      |      |
+ *                                 | Space|Backsp|------|       |------| Tab  |Enter |
+ *                                 |      |ace   | Esc  |       | Esc  |      |      |
+ *                                 `--------------------'       `--------------------'
  */
 // If it accepts an argument (i.e, is a function), it doesn't need KC_.
 // Otherwise, it needs KC_*
 [BASE] = KEYMAP(  // layer 0 : default
         // left hand
-        KC_EQL,         KC_1,         KC_2,   KC_3,   KC_4,   KC_5,   TG(MDIA),
-        KC_TAB,         KC_Q,         KC_W,   KC_E,   KC_R,   KC_T,   TG(SYMB),
-	KC_LCTL,        KC_A,         KC_S,   KC_D,   KC_F,   KC_G,
-        KC_LSFT,        CTL_T(KC_Z),  KC_X,   KC_C,   KC_V,   KC_B,   KC_LANG2,
-        LT(SYMB,KC_GRV),KC_LGUI,      LALT(KC_LSFT), KC_LEFT, KC_RIGHT,
-                                              ALT_T(KC_APP),  KC_LGUI,
-                                                              KC_HOME,
-                                             KC_SPC, KC_BSPC, KC_END,
+        KC_EQL,         KC_1,        KC_2,    KC_3,    KC_4,   KC_5,   TG(MDIA),
+        KC_TAB,         KC_Q,        KC_W,    KC_E,    KC_R,   KC_T,   TG(SYMB),
+	KC_LCTL,        KC_A,        KC_S,    KC_D,    KC_F,   KC_G,
+        KC_LSFT,        CTL_T(KC_Z)  KC_X,    KC_C,    KC_V,   KC_B,   KC_LANG2,
+        LT(MDIA,KC_GRV),KC_LGUI,     KC_LGUI, KC_LEFT, KC_RGHT,
+                                                               KC_ESC, KC_ESC,
+                                                                       KC_ESC,
+                                                       KC_SPC, KC_BSPC,KC_ESC,
         // right hand
-             KC_ESC,      KC_6,   KC_7,  KC_8,   KC_9,   KC_0,             KC_MINS,
-             TG(BASE),    KC_Y,   KC_U,  KC_I,   KC_O,   KC_P,             KC_BSLS,
-                          KC_H,   KC_J,  KC_K,   KC_L,   KC_SCLN,          KC_QUOT,
-             KC_LANG1,    KC_N,   KC_M,  KC_COMM,KC_DOT, KC_SLSH,          KC_RSFT,
-                                  KC_UP, KC_DOWN,KC_LBRC,KC_RBRC,          MO(MDIA),
-             KC_LALT, CTL_T(KC_ESC),
-             KC_PGUP,
-             KC_PGDN, KC_TAB, KC_ENT
+             KC_ESC,   KC_6,  KC_7,  KC_8,    KC_9,    KC_0,     KC_MINS,
+             TG(BASE), KC_Y,  KC_U,  KC_I,    KC_O,    KC_P,     KC_BSLS,
+                       KC_H,  KC_J,  KC_K,    KC_L,    KC_SCLN,  KC_QUOT,
+             KC_LANG1, KC_N,  KC_M,  KC_COMM, KC_DOT,  KC_SLSH,  KC_RALT,
+                              KC_UP, KC_DOWN, KC_LBRC, KC_RBRC,  MO(SYMB),
+             KC_ESC,   KC_ESC,
+             KC_ESC,
+             KC_ESC,   KC_TAB, KC_ENT
     ),
 /* Keymap 1: Symbol Layer
  *
@@ -91,14 +91,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                                KC_TRNS,
                                RGB_VAD,RGB_VAI,KC_TRNS,
        // right hand
-       KC_TRNS, KC_F6,   KC_F7,  KC_F8,   KC_F9,   KC_F10,  KC_F11,
+       KC_TRNS,  KC_F6,   KC_F7,  KC_F8,   KC_F9,   KC_F10,  KC_F11,
        TG(SYMB), KC_UP,   KC_7,   KC_8,    KC_9,    KC_ASTR, KC_F12,
-                KC_DOWN, KC_4,   KC_5,    KC_6,    KC_PLUS, KC_TRNS,
-       KC_TRNS, KC_AMPR, KC_1,   KC_2,    KC_3,    KC_BSLS, KC_TRNS,
-                         KC_TRNS,KC_DOT,  KC_0,    KC_EQL,  KC_TRNS,
-       RGB_TOG, RGB_SLD,
+                 KC_DOWN, KC_4,   KC_5,    KC_6,    KC_PLUS, KC_TRNS,
+       KC_TRNS,  KC_AMPR, KC_1,   KC_2,    KC_3,    KC_BSLS, KC_TRNS,
+                          KC_TRNS,KC_DOT,  KC_0,    KC_EQL,  KC_TRNS,
+       RGB_TOG,  RGB_SLD,
        KC_TRNS,
-       KC_TRNS, RGB_HUD, RGB_HUI
+       KC_TRNS,  RGB_HUD, RGB_HUI
 ),
 /* Keymap 2: Media and mouse keys
  *
