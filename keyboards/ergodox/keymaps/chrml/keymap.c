7d88f7a7676ace9816b53feb5f2570e00fcd6dce
fujiwara402 <fuj.t601216@gmail.com>
2017-07-10 19:10:59 +0900

    Swap BSLS and MINS

--- keyboards/ergodox/keymaps/chrml/keymap.c
+++ keyboards/ergodox/keymaps/chrml/keymap.c
@@ -52,8 +52,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                                                          KC_NO,
                                                      KC_SPC,  KC_BSPC,   KC_NO,
         // right hand
-             KC_MINS,  KC_6,  KC_7,   KC_8,    KC_9,    KC_0,     KC_EQL,
-             KC_RBRC,  KC_Y,  KC_U,   KC_I,    KC_O,    KC_P,     KC_BSLS,
+             KC_BSLS,  KC_6,  KC_7,   KC_8,    KC_9,    KC_0,     KC_EQL,
+             KC_RBRC,  KC_Y,  KC_U,   KC_I,    KC_O,    KC_P,     KC_MINS,
                        KC_H,  KC_J,   KC_K,    KC_L,    KC_SCLN,  KC_QUOT,
              KC_LANG1, KC_N,  KC_M,   KC_COMM, KC_DOT,  KC_SLSH,  KC_LSFT,
                               KC_LEFT,KC_DOWN, KC_UP,   KC_RGHT,  MO(SYMB),
