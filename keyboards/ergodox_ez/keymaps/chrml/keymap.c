e2ac4941efba09806d70fe3dbcc61d5311c109af
fujiwara402 <fuj.t601216@gmail.com>
2017-11-24 23:18:06 +0900

    add key for i3

--- keyboards/ergodox_ez/keymaps/chrml/keymap.c
+++ keyboards/ergodox_ez/keymaps/chrml/keymap.c
@@ -10,6 +10,7 @@
 #define UNDO RCTL(KC_Z)
 #define REDO RCTL(RSFT(KC_Z))
 #define PASTE LCTL(KC_V)
+#define FLOAT LSFT(LALT(KC_SPC)) 
 
 enum custom_keycodes {
   PLACEHOLDER = SAFE_RANGE, // can always be here
@@ -27,9 +28,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
  * | Ctrl   |   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |   ;: |   '"   |
  * |--------+------+------+------+------+------| Eisu |           | Kana |------+------+------+------+------+--------|
- * | LShift |Z/Ctrl|   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |   /? | RShift |
+ * | LShift |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |   /? | RShift |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   |  L2  |CtlSp | Alt  |  Alt | Esc  |                                       |  BS  | Hone |  End | PgDn | PgUp |
+ *   |  L2  |FLOAT |SftALT|  Alt | Esc  |                                       |  BS  | Hone |  End | PgDn | PgUp |
  *   `----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,-------------.  
  *                                        | UNDO | REDO |       |VolDwn| VolUp|
@@ -43,11 +44,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // Otherwise, it needs KC_*
 [BASE] = KEYMAP(  // layer 0 : default
         // left hand
-        KC_BSLS,  KC_1,          KC_2,     KC_3,    KC_4,   KC_5, S(KC_COMM),
-        KC_GRV,   KC_Q,          KC_W,     KC_E,    KC_R,   KC_T, KC_LBRC,
-        KC_LCTL,  KC_A,          KC_S,     KC_D,    KC_F,   KC_G,
-        KC_LSFT,  CTL_T(KC_Z),   KC_X,     KC_C,    KC_V,   KC_B, KC_LANG1,
-        MO(MDIA), LCTL(KC_LGUI), KC_LALT,  KC_LALT, KC_ESC,
+        KC_BSLS,  KC_1,   KC_2,          KC_3,    KC_4,   KC_5, S(KC_COMM),
+        KC_GRV,   KC_Q,   KC_W,          KC_E,    KC_R,   KC_T, KC_LBRC,
+        KC_LCTL,  KC_A,   KC_S,          KC_D,    KC_F,   KC_G,
+        KC_LSFT,  KC_Z,   KC_X,          KC_C,    KC_V,   KC_B, KC_LANG1,
+        MO(MDIA), FLOAT,  LSFT(KC_LALT), KC_LALT, KC_ESC,
                                                             UNDO,   REDO,
                                                                     TO(MDIA),
                                                     KC_SPC, KC_TAB, TO(SYMB),
@@ -55,7 +56,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
              S(KC_DOT),KC_6,  KC_7,   KC_8,    KC_9,    KC_0,    KC_MINS,
              KC_RBRC,  KC_Y,  KC_U,   KC_I,    KC_O,    KC_P,    KC_EQL,
                        KC_H,  KC_J,   KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-             KC_LANG2, KC_N,  KC_M,   KC_COMM, KC_DOT,  KC_SLSH, MO(SYMB),
+             KC_LANG2, KC_N,  KC_M,   KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
                               KC_BSPC,KC_HOME, KC_END,  KC_PGDN, KC_PGUP,
              KC_VOLD,  KC_VOLU,
              KC_MUTE,
